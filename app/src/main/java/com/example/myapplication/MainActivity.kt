4698be8d95fd94ed112a27c260386f13c8c45999
Mikolaj Karwowski <mikkarwowski@gmail.com>
2024-10-17 15:44:41 +0200

    Fix for some errors

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Any code that was added
+
+        // Changes made on master to fix production
     }
 }
